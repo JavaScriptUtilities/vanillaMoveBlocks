8e7fc3427a30b39a41d3435b7b19799f066bdfa1
Kevin Rocher <kevin@rocher-dev.com>
2016-10-06 19:18:51 +0200

    v 0.2.0
    * Add compressed version.
    * Perf fix at resize.

---
+++ grunt/uglify.js
@@ -0,0 +1,7 @@
+module.exports = {
+    my_target: {
+        files: {
+            'js/vanilla-moveblocks.min.js': ['js/vanilla-moveblocks.js']
+        }
+    }
+};
--- js/vanilla-moveblocks.js
+++ js/vanilla-moveblocks.js
@@ -1,31 +1,33 @@
 /*
  * Plugin Name: Vanilla-JS Move Blocks
- * Version: 0.1.0
+ * Version: 0.2.0
  * Plugin URL: https://github.com/Darklg/JavaScriptUtilities
  * JavaScriptUtilities Vanilla-JS may be freely distributed under the MIT license.
  */
 
 var vanillaMoveBlocks = function(el) {
     'use strict';
-    var self = this;
-    var currentPos = 10000;
-    var wrapper = false;
-    var winWidth = 0;
-    var positions = [];
+    var self = this,
+        currentPos = 10000,
+        posLength = 0,
+        resizeTimeout = false,
+        wrapper = false,
+        winWidth = 0,
+        positions = [];
 
     /* APP */
-    self.init = function() {
+    var init = function() {
         /* Add an initial wrapper around el */
-        self.wrapEl(el);
+        wrapEl(el);
         /* Get all positions */
         self.extractPositions();
         /* Change positions at init and resize */
-        self.resizeEvent();
-        window.addEventListener('resize', self.resizeEvent, 1);
+        self.setBlockPosition();
+        window.addEventListener('resize', resizeEvent, 1);
     };
 
     /* Wrap an item */
-    self.wrapEl = function(item) {
+    var wrapEl = function(item) {
         wrapper = document.createElement('div');
         wrapper.className = 'vmb-wrapper';
         item.parentNode.insertBefore(wrapper, item);
@@ -47,6 +49,7 @@ var vanillaMoveBlocks = function(el) {
             breakpoint: 10000,
             target: wrapper,
         });
+        posLength = positions.length;
         /* Sort positions */
         positions.sort(function sort_positions(a, b) {
             return b.breakpoint - a.breakpoint;
@@ -54,14 +57,20 @@ var vanillaMoveBlocks = function(el) {
     };
 
     /* Event at resize */
-    self.resizeEvent = function() {
+    var resizeEvent = function() {
+        clearTimeout(resizeTimeout);
+        resizeTimeout = setTimeout(self.setBlockPosition, 100);
+    };
+
+    /* Move block to correct position */
+    self.setBlockPosition = function() {
         // Get window width
         winWidth = window.innerWidth;
 
         // Find adapted view
         var tmpTarget = false,
             tmpPos = currentPos;
-        for (var i = 0, len = positions.length; i < len; i++) {
+        for (var i = 0; i < posLength; i++) {
             // keep that position if window width is higher than breakpoint
             if (winWidth > positions[i].breakpoint) {
                 continue;
@@ -71,15 +80,16 @@ var vanillaMoveBlocks = function(el) {
         }
 
         // If found position is different than current view
-        if (tmpPos != currentPos) {
-            // Move el to target
-            tmpTarget.appendChild(el);
-            // Save position
-            currentPos = tmpPos;
+        if (tmpPos == currentPos) {
+            return;
         }
+        // Move el to target
+        tmpTarget.appendChild(el);
+        // Save position
+        currentPos = tmpPos;
     };
 
     /* INIT */
-    self.init();
+    init();
     return self;
 };
